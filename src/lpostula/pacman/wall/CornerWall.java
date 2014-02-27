54850d40c083b87bcb8fc42f3082b03d5423bbf9
Loïs Postula <lpostula@ulb.ac.be>
2014-02-27 09:20:57 +0100

    Ajout du copyright

--- src/lpostula/pacman/wall/CornerWall.java
+++ src/lpostula/pacman/wall/CornerWall.java
@@ -1,3 +1,37 @@
+/*
+ * This file is part of SmartPacMan.
+ *
+ *     SmartPacMan is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     SmartPacMan is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with SmartPacMan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This file is part of SmartPacMan.
+ *
+ *     SmartPacMan is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     SmartPacMan is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with SmartPacMan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package lpostula.pacman.wall;
 
 import javafx.scene.canvas.Canvas;
